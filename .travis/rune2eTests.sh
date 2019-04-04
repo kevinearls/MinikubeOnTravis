57561e1d5243fdd0307c722506ce3c4c81ffb378
Kevin Earls <kearls@redhat.com>
2019-04-04 18:39:47 +0200

    Cleanup

--- .travis/rune2eTests.sh
+++ .travis/rune2eTests.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
-set -x
+#set -x
 
 # Confirm we're working
 kubectl get all --all-namespaces
 kubectl get ingress --all-namespaces
 minikube ip
 
-#- sleep 30
-curl $(minikube ip)/api/services
+# Loop as it sometimes takes a few seconds for the first trace to show up
 for i in {1..10}; do curl $(minikube ip)/api/services; echo ""; sleep 2; done
-curl $(minikube ip)/api/services --output fud.txt
-ls -alF
-grep jaeger-query fud.txt
+
